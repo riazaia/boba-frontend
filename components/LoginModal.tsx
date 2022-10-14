5aeff9dcda6eab00683861991a49c7d72996ca5d
Ria <riazaia.art@gmail.com>
2022-10-14 20:37:42 -0300

    Add checkbox to toggle password visibility

--- components/LoginModal.tsx
+++ components/LoginModal.tsx
@@ -1,117 +1,135 @@
-import {
-  Input,
-  ModalWithButtons
-} from "@bobaboard/ui-components";
+import { Input, ModalWithButtons } from "@bobaboard/ui-components";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import classnames from "classnames";
 import { useAuth } from "components/Auth";
 import { useHotkeys } from "react-hotkeys-hook";
 
 const LoginModal: React.FC<LoginModalProps> = (props) => {
-  const { isPending, isLoggedIn, attemptLogin, attemptLogout, authError } =
-    useAuth();
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const onSubmit = () => {
-    if (!isLoggedIn) {
-      attemptLogin!(email, password).then((success: boolean) => {
-        setPassword("");
-        if (success) {
-          setEmail("");
-          props.onCloseModal();
-        }
-      });
-    } else {
-      attemptLogout!().then(() => {
-        props.onCloseModal();
-      });
-    }
-  };
-  useHotkeys("enter", onSubmit, { enableOnTags: ["INPUT"] }, [email, password]);
+	const { isPending, isLoggedIn, attemptLogin, attemptLogout, authError } =
+		useAuth();
+	const [email, setEmail] = React.useState("");
+	const [password, setPassword] = React.useState("");
+	const onSubmit = () => {
+		if (!isLoggedIn) {
+			attemptLogin!(email, password).then((success: boolean) => {
+				setPassword("");
+				if (success) {
+					setEmail("");
+					props.onCloseModal();
+				}
+			});
+		} else {
+			attemptLogout!().then(() => {
+				props.onCloseModal();
+			});
+		}
+	};
+	useHotkeys("enter", onSubmit, { enableOnTags: ["INPUT"] }, [email, password]);
 
-  return (
-    <ModalWithButtons
-      isOpen={props.isOpen}
-      onCloseModal={props.onCloseModal}
-      onSubmit={onSubmit}
-      color={props.color}
-      primaryText={isLoggedIn ? "Logout" : "Login"}
-      primaryDisabled={
-        !isLoggedIn && (email.trim().length == 0 || password.length == 0)
-      }
-      secondaryText={"Cancel"}
-      shouldCloseOnOverlayClick={true}
-    >
-      <>
-        {!isLoggedIn && (
-          <div className="login">
-            <div className={classnames("inputs", { pending: isPending })}>
-              <div>
-                <Input
-                  id={"email"}
-                  value={email}
-                  label={"Email"}
-                  onTextChange={(text: string) => setEmail(text)}
-                  color={props.color}
-                />
-              </div>
-              <div>
-                <Input
-                  id={"password"}
-                  value={password}
-                  label={"Password"}
-                  onTextChange={(text: string) => setPassword(text)}
-                  password
-                  color={props.color}
-                />
-              </div>
-              <div className={classnames("error", { hidden: !authError })}>
-                {authError || "Hidden error field!"}
-              </div>
-            </div>
-          </div>
-        )}
-        {isLoggedIn && (
-          <div className="logout">
-            <div>Pull the trigger, Piglet. </div>
-          </div>
-        )}
-      </>
-      <style jsx>{`
-        .inputs {
-          margin: 0 auto;
-          margin-bottom: 15px;
-          width: 100%;
-        }
-        .inputs > div:first-child {
-          margin-bottom: 5px;
-        }
-        .buttons {
-          display: flex;
-          justify-content: flex-end;
-        }
-        .buttons > div {
-          margin-left: 15px;
-        }
-        .error {
-          color: red;
-          margin-top: 10px;
-          margin-left: 20px;
-          font-size: small;
-        }
-        .error.hidden {
-          visibility: hidden;
-        }
-      `}</style>
-    </ModalWithButtons>
-  );
+	const [togglePassword, setTogglePassword] = React.useState(true);
+
+	return (
+		<ModalWithButtons
+			isOpen={props.isOpen}
+			onCloseModal={props.onCloseModal}
+			onSubmit={onSubmit}
+			color={props.color}
+			primaryText={isLoggedIn ? "Logout" : "Login"}
+			primaryDisabled={
+				!isLoggedIn && (email.trim().length == 0 || password.length == 0)
+			}
+			secondaryText={"Cancel"}
+			shouldCloseOnOverlayClick={true}
+		>
+			<>
+				{!isLoggedIn && (
+					<div className="login">
+						<div className={classnames("inputs", { pending: isPending })}>
+							<div>
+								<Input
+									id={"email"}
+									value={email}
+									label={"Email"}
+									onTextChange={(text: string) => setEmail(text)}
+									color={props.color}
+								/>
+							</div>
+							<div>
+								<Input
+									id={"password"}
+									value={password}
+									label={"Password"}
+									onTextChange={(text: string) => setPassword(text)}
+									password={togglePassword}
+									color={props.color}
+								/>
+								<p className={"password-toggle"}>
+									<input
+										type="checkbox"
+										id="password-toggle"
+										onClick={() => setTogglePassword(!togglePassword)}
+									/>
+									<FontAwesomeIcon
+										icon={togglePassword ? faEye : faEyeSlash}
+										className={classnames("icon")}
+										title={"label"}
+									/>
+									<label htmlFor="password-toggle"> Show Password</label>
+								</p>
+							</div>
+							<div className={classnames("error", { hidden: !authError })}>
+								{authError || "Hidden error field!"}
+							</div>
+						</div>
+					</div>
+				)}
+				{isLoggedIn && (
+					<div className="logout">
+						<div>Pull the trigger, Piglet. </div>
+					</div>
+				)}
+			</>
+			<style jsx>{`
+				.inputs {
+					margin: 0 auto;
+					margin-bottom: 15px;
+					width: 100%;
+				}
+				.inputs > div:first-child {
+					margin-bottom: 5px;
+				}
+				.buttons {
+					display: flex;
+					justify-content: flex-end;
+				}
+				.buttons > div {
+					margin-left: 15px;
+				}
+				.error {
+					color: red;
+					margin-top: 10px;
+					margin-left: 20px;
+					font-size: small;
+				}
+				.error.hidden {
+					visibility: hidden;
+				}
+				.password-toggle {
+					cursor: pointer;
+					font-size: 2rem;
+				}
+			`}</style>
+		</ModalWithButtons>
+	);
 };
 
 export interface LoginModalProps {
-  isOpen: boolean;
-  onCloseModal: () => void;
-  color?: string;
+	isOpen: boolean;
+	onCloseModal: () => void;
+	color?: string;
 }
 
 export default LoginModal;
